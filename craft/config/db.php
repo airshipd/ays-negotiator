f202cbc14ba802d5bfbd56a651592ddb44d4a28a
Jerry Chai <jerry@gooddogdesign.com>
2017-09-14 09:48:18 -0700

    remove extra db configs

--- craft/config/db.php
+++ craft/config/db.php
@@ -17,29 +17,5 @@ return array(
 		'password' => getenv('DB_PASS'),
 		'database' => getenv('DB_NAME'),
 		'initSQLs' => array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
-	),
-
-	'139.59.111.16' => array (
-		'server' => '127.0.0.1',
-		'database' => 'negotiator',
-		'user' => 'root',
-		'password' => '509e8e818eb2b9aacb621c9dece7493b8726a46675989c37',
-		'initSQLs' => array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
-	),
-
-	'moneyforcar.com.au' => array (
-		'server' => '127.0.0.1',
-		'database' => 'negotiator',
-		'user' => 'root',
-		'password' => '509e8e818eb2b9aacb621c9dece7493b8726a46675989c37',
-		'initSQLs' => array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
-	),
-
-	'.local' => array(
-		'server' => '127.0.0.1',
-		'database' => 'negotiator',
-		'user' => 'root',
-		'password' => '',
 	)
-
 );
