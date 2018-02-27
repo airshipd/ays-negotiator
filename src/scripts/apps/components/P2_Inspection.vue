35852af3488d0fb22402143810e0d21340f6e16c
Ilya Savinkov <staliniv@gmail.com>
2018-02-27 17:46:48 +0500

    trello-38: Remove "skip to paperwork" on pre-inspection form

--- src/scripts/apps/components/P2_Inspection.vue
+++ src/scripts/apps/components/P2_Inspection.vue
@@ -141,7 +141,6 @@
     </div>
 
     <b1-button :label="'Submit'" :action="submitForm" :fullWidth="true"></b1-button>
-    <b1-button class="grey lighten-1" :label="'Skip to Paperwork'" :action="skip" :fullWidth="true"></b1-button>
   </section>
 </template>
 
@@ -223,9 +222,6 @@ export default {
                     that.inspection.licenseAndRegistrationPhotos.push(blob)
                 }
             });
-        },
-        skip() {
-            this.$router.push('/final/1/' + this.$route.params.id)
         },
         submitForm() {
             this.$validator.validateAll().then((result) => {
