d3d2829edea52a8a878a0de5d0c6d1a13c3d1e1d
Ilya Savinkov <staliniv@gmail.com>
2018-04-17 15:23:59 +0500

    trello-113: Adjust the follow-up email notification text

--- craft/plugins/finalizer/services/Finalizer_EmailService.php
+++ craft/plugins/finalizer/services/Finalizer_EmailService.php
@@ -88,12 +88,14 @@ class Finalizer_EmailService extends BaseApplicationComponent
             return;
         }
 
+        $car = craft()->finalizer_fields->getCarFullName($inspection);
+
         //get negotiator name to embed into the template
         $user = craft()->userSession->getUser();
         if($user && $user->isInGroup('negotiators')) {
-            $first_line = $user->getFullName() . ' has just sent you a car to follow up';
+            $first_line = $user->getFullName() . " has just sent you $car to follow up";
         } else {
-            $first_line = 'This car has been auto sent to you to follow up';
+            $first_line = "$car has been auto sent to you to follow up";
         }
 
         // get email body
--- craft/plugins/finalizer/templates/email/inspection_follow_up.php
+++ craft/plugins/finalizer/templates/email/inspection_follow_up.php
@@ -1,3 +1,3 @@
 <?= $first_line ?>, don't forget to check and follow up as soon as possible.
-If it is not followed up within 48 hours, it will automatically be assigned to another sales consultant.
+If it is not followed up within 72 hours, it will automatically be assigned to another sales consultant.
 No-one likes losing commission ;)
